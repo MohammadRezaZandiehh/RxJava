a2b5116d6d42983281b27b82befb37d67d01638f
mohammad <m.zandieh1999@gmail.com>
2021-04-30 17:48:59 +0430

    add Observable & Observer

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.rxjava;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Disposable disposable;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("Tiger", "Elephent", "Lion", "Bear")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        //vaghti movafagh shodim moshtarak beshim rooye obserable  zade mishe .
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        //vaghti heyvani dide shod esme heyvan seda zade mishe .
+                        Log.d(TAG, "onNext: ");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        // heyne anjame observe kakhataei b vojod biad .
+                        Log.d(TAG, "onError: " + e.toString());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        //dar bala vaghti akharin esme ersal shod (yani too inja "Bear") in method seda zade mishe .
+                        Log.d(TAG, "onComplete: ");
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
